bf690a67261e8fd7bb34feaada0b6baf27470715
Mritunjay kumar <mritunjaykr160@gmail.com>
2024-12-17 00:16:01 +0530

    updated main route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import { Blog } from "./pages/Blog";
@@ -9,6 +9,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/signup" />}></Route>
           <Route path="/signup" element={<Signup />}></Route>
           <Route path="signin" element={<Signin />}></Route>
           <Route path="/blog/:id" element={<Blog />}></Route>
